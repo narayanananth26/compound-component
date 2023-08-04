37709137af257b40314a726d439b672450c98ac2
Ananth Narayan <narayanananth26@gmail.com>
2023-08-04 13:25:09 +0530

    implement compound component pattern

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ export default function App() {
 	return (
 		<div>
 			<h1>Compound Component Pattern</h1>
-			<Counter
+			{/* <Counter
 				iconIncrease="+"
 				iconDecrease="-"
 				label="My NOT so flexible counter"
 				hideLabel={false}
 				hideIncrease={false}
 				hideDecrease={false}
-			/>
+			/> */}
+
+			<Counter>
+				<Counter.Label>My super flexible counter</Counter.Label>
+				<Counter.Increase icon="+" />
+				<Counter.Count />
+				<Counter.Decrease icon="-" />
+			</Counter>
 		</div>
 	);
 }
